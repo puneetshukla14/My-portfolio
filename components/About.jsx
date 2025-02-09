a48631a007541c238bbb97872d67afd5367439cc
Puneet Shukla <106764262+puneetshukla14@users.noreply.github.com>
2025-02-09 22:31:43 +0530

    .

---
+++ components/About.jsx
@@ -0,0 +1,90 @@
+import Link from "next/link";
+import { motion } from "framer-motion";
+
+import { socials } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import EmailIcon from "./../public/assets/icons/email.svg";
+
+function About() {
+  return (
+    <section
+      className="md:my-36 md:w-2/3 w-full h-full xl:ml-36 lg:ml-12 p-8 md:mt-[40svh] xl:mt-[150px]"
+      id="about"
+    >
+      <motion.div
+        variants={textVariant()}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true, amount: 0.25 }}
+      >
+        <p className={"sectionSubText text-gray-300"}>Introduction</p>
+        <h2 className={"sectionHeadText text-white"}>Overview.</h2>
+      </motion.div>
+      <motion.div
+        variants={fadeIn("", "", 0.1, 1)}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true, amount: 0.25 }}
+        className="mt-4 dark:text-ctnSecondaryDark text-gray-300 text-[17px] w-full leading-[30px] flex flex-col justify-between gap-6"
+      >
+        <div>
+        I am Puneet Shukla a software engineer with a strong background in full-stack development, AI, and machine learning.
+         Skilled in C++, Java, Python, JavaScript, and TypeScript, I specialize in building scalable web applications 
+         using React.js, Angular, Next.js, Node.js, and Express.js, along with database technologies like MongoDB, MySQL, 
+         and Firebase. My experience includes developing an AI-driven chatbot using NLP at Medanta The Medicity, optimizing 
+         RESTful APIs, and working on innovative projects like a Threads clone and a Disney+ Hotstar clone. My research on Emotional
+          Intelligence in AI Chatbots explores AI’s ability to detect and respond to human emotions dynamically, addressing challenges 
+          like bias, multilingual adaptation, and ethical concerns. Passionate about cutting-edge technology and AI-driven solutions, 
+          I am always eager to collaborate, innovate, and contribute to impactful projects.
+          <br className="sm:block hidden" />
+          Let&apos;s collaborate to bring your ideas to life!
+        </div>
+        <div className="w-fit break-words">
+          <Link
+            href="puneetshukla043@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+            className="hover:text-primary w-full transition-all duration-100 ease-in flex md:items-center gap-2 md:flex-row flex-wrap word-break hover:-translate-y-2"
+          >
+            <EmailIcon className="w-[30px] h-[30px]" />
+            puneetshukla043@gmail.com
+          </Link>
+        </div>
+        <div className="flex gap-5 items-center">
+          {socials.map((social) => (
+            <Link
+              href={social.link}
+              target="_blank"
+              key={social.id}
+              className="w-8 h-8 hover:-translate-y-2 ease-in transition-all duration-100 cursor-pointer"
+            >
+              {social.icon}
+            </Link>
+          ))}
+        </div>
+        <Link
+          href="document\Puneet_Shukla_Resume.pdf"
+          target="_blank"
+          rel="noreferrer"
+          className="w-fit"
+        >
+          <div className="btn w-fit bg-tertiary text-white px-7 py-2 rounded-md overflow-hidden relative cursor-pointer">
+            <div className="original bg-primary text-white px-7 py-2">
+              Resume
+            </div>
+            <div className="letters">
+              <span>R</span>
+              <span>e</span>
+              <span>s</span>
+              <span>u</span>
+              <span>m</span>
+              <span>e</span>
+            </div>
+          </div>
+        </Link>
+      </motion.div>
+    </section>
+  );
+}
+
+export default About;
---
+++ constants/index.js
@@ -0,0 +1,535 @@
+import GithubIcon from "./../public/assets/icons/github.svg";
+import LinkedInIcon from "./../public/assets/icons/linkedin.svg";
+import XIcon from "./../public/assets/icons/x.svg";
+import InstagramIcon from "./../public/assets/icons/instagram.svg";
+import FrontendIcon from "./../public/assets/icons/frontend.svg";
+import LeaderShipIcon from "./../public/assets/icons/leadership.svg";
+import ProblemSolvingIcon from "./../public/assets/icons/problem-solving.svg";
+import FreelancerIcon from "./../public/assets/icons/freelance.svg";
+import BackendIcon from "./../public/assets/icons/backend.svg";
+import FullStackIcon from "./../public/assets/icons/full-stack.svg";
+
+const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "skills",
+    title: "Skills",
+  },
+  {
+    id: "projects",
+    title: "Projects",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Software Developer",
+    icon: <FullStackIcon />,
+  },
+  {
+    title: "Frontend Developer",
+    icon: <FrontendIcon />,
+  },
+  // {
+  //   title: "Backend Developer",
+  //   icon: <BackendIcon />,
+  // },
+  {
+    title: "Problem Solving",
+    icon: <ProblemSolvingIcon />,
+  },
+  {
+    title: "Freelancer",
+    icon: <FreelancerIcon />,
+  },
+  // {
+  //   title: "Leadership",
+  //   icon: <LeaderShipIcon />,
+  // },
+];
+
+const technologies = {
+  languages: [
+    {
+      name: "HTML5",
+      icon: "/assets/tech/html5.svg",
+      link: "https://html.spec.whatwg.org/multipage/",
+    },
+    {
+      name: "CSS3",
+      icon: "/assets/tech/css3.svg",
+      link: "https://www.w3.org/Style/CSS/Overview.en.html",
+    },
+    {
+      name: "JavaScript",
+      icon: "/assets/tech/javascript.svg",
+      link: "https://262.ecma-international.org/",
+    },
+    {
+      name: "TypeScript",
+      icon: "/assets/tech/typescript.svg",
+      link: "https://www.typescriptlang.org/",
+    },
+    {
+      name: "C",
+      icon: "/assets/tech/c.svg",
+      link: "https://en.cppreference.com/w/c",
+    },
+    {
+      name: "Java",
+      icon: "/assets/tech/java.svg",
+      link: "https://www.java.com/en/",
+    },
+    {
+      name: "Python",
+      icon: "/assets/tech/python.svg",
+      link: "https://www.python.org/",
+    },
+  ],
+  frameworks: [
+    {
+      name: "Next.js",
+      icon: "/assets/tech/nextjs.svg",
+      link: "https://nextjs.org/",
+    },
+    {
+      name: "TailwindCSS",
+      icon: "/assets/tech/tailwindcss.svg",
+      link: "https://tailwindcss.com/",
+    },
+    {
+      name: "Express.js",
+      icon: "/assets/tech/expressjs.png",
+      link: "https://expressjs.com/",
+    },
+    {
+      name: "Flutter",
+      icon: "/assets/tech/flutter.svg",
+      link: "https://flutter.dev/",
+    },
+  ],
+  libraries: [
+    {
+      name: "React",
+      icon: "/assets/tech/react.svg",
+      link: "https://react.dev/",
+    },
+    {
+      name: "Three.js",
+      icon: "/assets/tech/threejs.svg",
+      link: "https://threejs.org/",
+    },
+    {
+      name: "Styled-Components",
+      icon: "/assets/tech/styled-components.png",
+      link: "https://styled-components.com/",
+    },
+    {
+      name: "Framer-motion",
+      icon: "/assets/tech/framer.svg",
+      link: "https://www.framer.com/motion/",
+    },
+    {
+      name: "Zustand",
+      icon: "https://user-images.githubusercontent.com/958486/218346783-72be5ae3-b953-4dd7-b239-788a882fdad6.svg",
+      link: "https://zustand-demo.pmnd.rs",
+    },
+    {
+      name: "Redux/Redux-toolkit",
+      icon: "https://raw.githubusercontent.com/reduxjs/redux/master/logo/logo.png",
+      link: "https://redux.js.org",
+    },
+    {
+      name: "NextAuth.js",
+      icon: "/assets/tech/nextauthjs.png",
+      link: "https://next-auth.js.org/",
+    },
+    {
+      name: "Prisma",
+      icon: "/assets/tech/prisma.svg",
+      link: "https://www.prisma.io/",
+    },
+  ],
+  tools: [
+    {
+      name: "Git",
+      icon: "/assets/tech/git.svg",
+      link: "https://git-scm.com/",
+    },
+    {
+      name: "Github",
+      icon: "/assets/icons/github.svg",
+      link: "https://github.com/",
+    },
+    {
+      name: "Postman",
+      icon: "/assets/tech/postman.svg",
+      link: "https://www.postman.com/",
+    },
+    {
+      name: "Figma",
+      icon: "/assets/tech/figma.svg",
+      link: "https://www.figma.com/",
+    },
+    {
+      name: "Docker",
+      icon: "/assets/tech/docker.svg",
+      link: "https://www.docker.com/",
+    },
+  ],
+  environments: [
+    {
+      name: "Node.js",
+      icon: "/assets/tech/nodejs.svg",
+      link: "https://nodejs.org/en",
+    },
+  ],
+  databases: [
+    {
+      name: "MySQL",
+      icon: "/assets/tech/my-sql.png",
+      link: "https://www.mysql.com/",
+    },
+    {
+      name: "PostgreSQL",
+      icon: "/assets/tech/postgresql.png",
+      link: "https://www.postgresql.org",
+    },
+    {
+      name: "MongoDB",
+      icon: "/assets/tech/mongodb.svg",
+      link: "https://www.mongodb.com/",
+    },
+    {
+      name: "Firebase",
+      icon: "/assets/tech/firebase.svg",
+      link: "https://firebase.google.com/",
+    },
+  ],
+};
+
+const experiences = [
+  // {
+  //   title: "Full Stack Developer",
+  //   company_name: "Tech Lead at GDSC MVJCE",
+  //   icon: "/assets/company/gdsc-logo.svg",
+  //   iconBg: "#E6DEDD",
+  //   date: "July 2023 - Present",
+  //   points: [
+  //     "Currently serving as the Tech Lead of GDSC, responsible for guiding technical aspects, fostering collaboration, and contributing to strategic decisions.",
+  //     "Led induction programs, ensured new member alignment with GDSC's mission, and provided technical mentorship.",
+  //     "Developed GDSC MVJCE website, enhancing the organization's online visibility.",
+  //     "Spearheaded promotional campaigns, actively engaged with the GDSC community, and played a key role in planning and executing events.",
+  //   ],
+  // },
+  {
+    title: "Software Developer Intern",
+    company_name: "Medanta The Medicity",
+    icon: "/assets/company/curowell-logo.svg",
+    iconBg: "#E6DEDD",
+    date: "November 2023",
+    points: [
+      "Developing a healthcare SaaS platform using Next.js, TypeScript, and Tailwind CSS, significantly enhancing user experience.",
+      "Maintaining an efficient codebase under senior guidance, ensuring adherence to best practices in software development.",
+      "Contributing to the DND form microfrontend, improving user interaction by 30%.",
+      "Collaborating in UI/UX design efforts, leading to a 25% reduction in component development time.",
+    ],
+  },
+  
+  // {
+  //   title: "Frontend Developer",
+  //   company_name: "Tech team member at GDSC MVJCE",
+  //   icon: "/assets/company/gdsc-logo.svg",
+  //   iconBg: "#E6DEDD",
+  //   date: "February 2022 - July 2023",
+  //   points: [
+  //     "Successfully planned and executed induction programs, providing new members with crucial information about the organization's goals and values.",
+  //     "Demonstrated web development expertise by conducting CSS workshops, equipping participants with practical web design and styling skills.",
+  //     "Played a crucial role in promoting GDSC events and actively collaborated with cross-functional teams to deliver high-quality projects, fostering teamwork and innovation.",
+  //     "Actively engaged with the GDSC community, assisted in workshops, and contributed to planning and managing GDSC events, ensuring their success and growth.",
+  //   ],
+  // },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Threads Clone",
+    description:
+      "Developed a Full-Stack Threads Clone using Next.js 14+, TypeScript, and MongoDB, implementing features like real-time notifications, nested comments, authentication, and optimized search for enhanced user engagement and performance. Engineered a scalable and responsive UI with Tailwind CSS, Shadcn UI, and Figma-based designs, integrating server-less APIs, role-based community management, and file uploads to ensure seamless interaction and accessibility",
+    tags: [
+      {
+        name: "typescript",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "next.js",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwindcss",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "shadcn",
+        color: "orange-text-gradient",
+      },
+      {
+        name: "cloudinary",
+        color: "yellow-text-gradient",
+      },
+    ],
+    image: "/assets/projects/threads.png",
+    
+    source_code_link: "https://github.com/puneetshukla14/Threads-main",
+    deployed_link: "https://threads-one-theta.vercel.app/",
+  },
+  {
+    name: "DisneyHotstarClone",
+    description:
+      "Project Highlight: Disney Hotstar Clone As a dedicated software developer, I undertook the challenge of building a Disney Hotstar Clone from the ground up and successfully deployed it on GitHub. This project exemplifies my expertise in full-stack development, user experience design, and cloud deployment. ",
+    tags: [
+      {
+        name: "typescript",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "next.js",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwindcss",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "fabricjs",
+        color: "orange-text-gradient",
+      },
+      {
+        name: "liveblocks",
+        color: "yellow-text-gradient",
+      },
+    ],
+    image: "/assets/projects/disney.png",
+    source_code_link: "https://github.com/puneetshukla14/DisneyHotstarClone",
+    deployed_link: "https://disneyhotstar-henna.vercel.app/",
+  },
+  // {
+  //   name: "GDSC Website",
+  //   description:
+  //     "Welcome to the official repository for the GDSC MVJCE Website, built with Next.js, Styled Components, and Prisma! This website serves as the central hub for all things tech-related at GDSC MVJCE. From insightful blog posts to incredible community projects, and from exciting tech events to getting to know our team, you'll find it all here.",
+  //   tags: [
+  //     {
+  //       name: "next.js",
+  //       color: "blue-text-gradient",
+  //     },
+  //     {
+  //       name: "styled-components",
+  //       color: "green-text-gradient",
+  //     },
+  //     {
+  //       name: "three.js",
+  //       color: "pink-text-gradient",
+  //     },
+  //     {
+  //       name: "prisma",
+  //       color: "orange-text-gradient",
+  //     },
+  //     {
+  //       name: "framer-motion",
+  //       color: "yellow-text-gradient",
+  //     },
+  //   ],
+  //   image: "/assets/projects/gdsc-website.png",
+  //   source_code_link: "https://github.com/GDSC-MVJCE/gdscmvjce-website.git",
+  //   deployed_link: "https://gdscmvjce.vercel.app/",
+  // },
+  {
+    name: "Research Ppaer on Emotional Intelligence In-AI Chatbot ",
+    description:
+      "This research explores integrating emotional intelligence in AI chatbots, enabling them to detect, interpret, and respond to human emotions dynamically. It addresses challenges like bias, multilingual adaptation, data privacy, and ethics. Real-world applications in healthcare, education, and customer service are analyzed. ",
+    tags: [
+      {
+        name: "NLP",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Emotional Intelligence",
+        color: "green-text-gradient",
+      },
+      {
+        name: "HCI",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Robotics",
+        color: "orange-text-gradient",
+      },
+      {
+        name: "Python",
+        color: "yellow-text-gradient",
+      },
+    ],
+    image: "/assets/projects/paper.png",
+    source_code_link:
+      "https://github.com/puneetshukla14/Research_Ppaer_Emotional_Intelligence_In-AI_Chatbot",
+    deployed_link:
+      "https://zenodo.org/records/14838063",
+  },
+  // {
+  //   name: "Netflix Clone",
+  //   description:
+  //     "Netflix-Clone is a fully responsive web application that replicates the popular streaming platform Netflix. It is built using Next.js, Prisma, React, NextAuth.js, Tailwind CSS, MongoDB and TypeScript. This project aims to showcase the implementation of these technologies to create a feature-rich clone of Netflix.",
+  //   tags: [
+  //     {
+  //       name: "next.js",
+  //       color: "blue-text-gradient",
+  //     },
+  //     {
+  //       name: "typescript",
+  //       color: "green-text-gradient",
+  //     },
+  //     {
+  //       name: "tailwindcss",
+  //       color: "pink-text-gradient",
+  //     },
+  //     {
+  //       name: "mongodb",
+  //       color: "orange-text-gradient",
+  //     },
+  //   ],
+  //   image: "/assets/projects/netflix-clone.png",
+  //   source_code_link: "https://github.com/Shivam-Sharma-1/Netflix-Clone.git",
+  //   deployed_link: "https://netflix-clone12345.vercel.app",
+  // },
+  // {
+  //   name: "Van-Life",
+  //   description:
+  //     "VanLife is a user-friendly website built with React that simplifies van searches. It is a web app used to browse different types of vans. It allows users to log in and offers convenient tag-based filtering for easy browsing.",
+  //   tags: [
+  //     {
+  //       name: "react",
+  //       color: "blue-text-gradient",
+  //     },
+  //     {
+  //       name: "mirage.js",
+  //       color: "green-text-gradient",
+  //     },
+  //     {
+  //       name: "firebase-firestore",
+  //       color: "pink-text-gradient",
+  //     },
+  //   ],
+  //   image: "/assets/projects/vanlife.png",
+  //   source_code_link: "https://github.com/Shivam-Sharma-1/Van-Life.git",
+  //   deployed_link: "https://myvanlife.netlify.app",
+  // },
+  // {
+  // 	name: "Spooky-Run",
+  // 	description:
+  // 		"Spooky Run is a web based 2D arcade style game that allows players to play as a lost dog finding its way back home through a spooky forest. The game is built using vanilla JavaScript and utilizes HTML5 and the 'canvas' element to render graphics and handle user input.",
+  // 	tags: [
+  // 		{
+  // 			name: "html5",
+  // 			color: "blue-text-gradient"
+  // 		},
+  // 		{
+  // 			name: "css3",
+  // 			color: "green-text-gradient"
+  // 		},
+  // 		{
+  // 			name: "javascript",
+  // 			color: "pink-text-gradient"
+  // 		}
+  // 	],
+  // 	image: "/assets/projects/spooky-run.png",
+  // 	source_code_link: "https://github.com/Shivam-Sharma-1/Spooky-Run.git",
+  // 	deployed_link: "https://shivam-sharma-1.github.io/Spooky-Run"
+  // }
+];
+
+const socials = [
+  {
+    id: "github",
+    icon: <GithubIcon />,
+    link: "https://github.com/puneetshukla14?tab=repositories",
+  },
+  {
+    id: "linkedin",
+    icon: <LinkedInIcon />,
+    link: "https://www.linkedin.com/in/puneet-shukla-72b915225/",
+  },
+  {
+    id: "x",
+    icon: <XIcon />,
+    link: "",
+  },
+  {
+    id: "instagram",
+    icon: <InstagramIcon />,
+    link: "https://www.instagram.com/puneet_146/",
+  },
+];
+
+const heroTexts = [
+  "React/Next.js developer",
+  500,
+  "Freelancer",
+  500,
+  "Full-Stack developer",
+  500,
+  "Frontend developer",
+  500,
+  "Backend developer",
+  500,
+  "Problem solver",
+  500,
+];
+
+export {
+  navLinks,
+  services,
+  technologies,
+  experiences,
+  testimonials,
+  projects,
+  socials,
+  heroTexts,
+};
